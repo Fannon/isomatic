fb9d12a13e8a5e6690edef0b5b09c4a07cc021ba
Fannon <heimlersimon@gmail.com>
2013-11-26 16:33:06 +0100

    Customizing Foundation Adding JavaScript minification via Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,58 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+        uglify: {
+            my_target: {
+                files: {
+                    'js/app.min.js': [
+                        'bower_components/foundation/js/foundation.min.js',
+                        'bower_components/underscore/underscore-min.js',
+                        'bower_components/backbone/backbone-min.js',
+                        'lib/innersvg.js',
+                        'js/main.js',
+                        'js/options.js',
+                        'js/ui.js',
+                        'js/data.js',
+                        'js/isotypeLayout.js',
+                        'js/visualisation.js',
+                        'js/defaultIcons.js'
+                    ]
+                }
+            }
         },
-        files: {
-          'css/app.css': 'scss/app.scss'
-        }        
-      }
-    },
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+        sass: {
+            options: {
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
+        },
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass']
+            }
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
-}
+//            js:  { files: 'js/*.js', tasks: [ 'uglify' ] }
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['sass', 'uglify']);
+    grunt.registerTask('default', ['build', 'watch']);
+};
